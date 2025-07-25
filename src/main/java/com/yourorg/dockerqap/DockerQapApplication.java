baafa3bfe0e461a48b24bb90d2ca958125332229
NSparkes95 <166650210+NSparkes95@users.noreply.github.com>
2025-07-24 22:13:47 -0230

    Add Spring Boot application configuration and update dependencies

--- src/main/java/com/yourorg/dockerqap/DockerQapApplication.java
+++ src/main/java/com/yourorg/dockerqap/DockerQapApplication.java
@@ -3,7 +3,9 @@ package com.yourorg.dockerqap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class DockerQapApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DockerQapApplication.class, args);
     }
