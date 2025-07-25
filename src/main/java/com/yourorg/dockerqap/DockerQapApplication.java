940023b30ef0b2cdec1d385d27e9d915049cda39
NSparkes95 <166650210+NSparkes95@users.noreply.github.com>
2025-07-24 22:09:46 -0230

    Set up Docker environment with MySQL and Spring Boot configuration

--- src/main/java/com/yourorg/dockerqap/DockerQapApplication.java
+++ src/main/java/com/yourorg/dockerqap/DockerQapApplication.java
@@ -1,4 +1,10 @@
 package com.yourorg.dockerqap;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 public class DockerQapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerQapApplication.class, args);
+    }
 }
