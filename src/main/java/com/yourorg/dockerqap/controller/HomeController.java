a2356a3fa2c54030d8b0e1b79d52b3e231892165
NSparkes95 <166650210+NSparkes95@users.noreply.github.com>
2025-07-31 13:48:04 -0230

    Add Spring Boot application configuration and update dependencies

---
+++ src/main/java/com/yourorg/dockerqap/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.yourorg.dockerqap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Golf Tournament API is running!";
+    }
+}
